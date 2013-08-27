a07d86e31ee45097fc0f88eddc0fd57bb68eb442
Tony Petty <tpetty@appmobi.com>
2013-08-27 05:48:08 -0400

    Changes.

--- buttons.php
+++ buttons.php
@@ -6,28 +6,28 @@ $html = <<<HTML
 	</div>
 	<div data-role="content">
 		<div data-role="collapsible-set" data-theme="c" data-content-theme="d">
-			<div data-role="collapsible">
+			<div data-role="collapsible" data-collapsed="true">
 				<h3>Twitter</h3>
 				<button onclick="twitterAvail();">Available</button>
 				<button id="tweet" disabled="disabled" onclick="twitterTweet();">Tweet</button>
 			</div>
-			<div data-role="collapsible">
+			<div data-role="collapsible" data-collapsed="true">
 				<h3>Optimize</h3>
 				<button onclick="doOptimize();">Do Optimize</button>
 			</div>
-			<div data-role="collapsible">
+			<div data-role="collapsible" data-collapsed="true">
 				<h3>Login</h3>
 				<button onclick="login();">Login</button>
 				<button onclick="login('silent', {'provider' : 'tpettytest','provider_id' : '12344321','email' : 'tpettytest@appmobi.com','screen_name' : 'Tony Petty'});">Silent Login</button>
 				<button id="logout_button" onclick="logout();">Logout</button>
 				<div id="message"></div>
 			</div>
-			<div data-role="collapsible" data-collapsed="false">
+			<div data-role="collapsible" data-collapsed="true">
 				<h3>1Touch</h3>
 				<button onclick="onetouch();">Purchase</button>
 				<button onclick="onetouch_restore();">Restore</button>
 			</div>
-			<div data-role="collapsible">
+			<div data-role="collapsible" data-collapsed="true">
 				<h3>Debug</h3>
 				<button onclick="ddebug('debug message');">Send Debug Message</button>
 			</div>
