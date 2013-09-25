d33e8f0b81addc90fa4c32b6a83171d119efec31
Tony Petty <tpetty@appmobi.com>
2013-09-25 20:13:43 +0000

    Updated location script.

--- location.php
+++ location.php
@@ -1,4 +1,11 @@
 <?php
-print_r($_REQUEST);
-print_r($_SERVER);
+switch ($_SERVER['REMOTE_ADDR']) {
+case '66.202.133.210':
+	$location = 'office';
+	break;
+default:
+	$location = 'home';
+	break;
+}
+echo $_GET['callback'] . '("' . $location . '");';
 ?>
