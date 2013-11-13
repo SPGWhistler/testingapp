8751287893f13338750c69f8b239994f120eda84
Tony Petty <tpetty@appmobi.com>
2013-11-13 14:42:28 +0000

    Changes.

--- location.php
+++ location.php
@@ -1,8 +1,12 @@
 <?php
 switch ($_SERVER['REMOTE_ADDR']) {
+case '67.241.136.186':
 case '108.176.92.251':
 case '66.202.133.210':
 	$location = 'office';
+	break;
+case '23.30.219.169':
+	$location = 'office2';
 	break;
 default:
 	$location = 'home';
